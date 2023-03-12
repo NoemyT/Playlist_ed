f8f0c2c841819f3f67db04c127fb348aa13e46d3
Noemy Torres <noemy_torres9@hotmail.com>
2023-03-12 20:51:00 -0300

    Função de adicionar

--- Playlist_cd.py
+++ Playlist_cd.py
@@ -9,3 +9,16 @@ class Playlist:
     def __init__(self):
         self.head = None
         self.tail = None
+
+    def adc_mus(self):
+        titulo = input("\nNome da música: ")
+        artista = input("Nome do artista: ")
+        new_node = Musica(titulo, artista)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+        print(f"\n{titulo} - {artista} adicionada a playlist!")        
