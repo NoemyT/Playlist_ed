92af342f5dbe68c740ba6c142aad78b3cade2118
Noemy Torres <noemy_torres9@hotmail.com>
2023-03-12 20:36:18 -0300

    Criação das classes

---
+++ Playlist_cd.py
@@ -0,0 +1,11 @@
+class Musica:
+    def __init__(self, titulo, artista, prev=None, next=None):
+        self.titulo = titulo
+        self.artista = artista
+        self.prev = prev
+        self.next = next
+
+class Playlist:
+    def __init__(self):
+        self.head = None
+        self.tail = None
