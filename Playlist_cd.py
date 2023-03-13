3fe3b8e13eba1d31ee2d8a27c2952f85db583ed7
Noemy Torres <noemy_torres9@hotmail.com>
2023-03-12 21:05:43 -0300

    Função de apagar música da playlist

--- Playlist_cd.py
+++ Playlist_cd.py
@@ -21,4 +21,24 @@ class Playlist:
             new_node.prev = self.tail
             self.tail.next = new_node
             self.tail = new_node
-        print(f"\n{titulo} - {artista} adicionada a playlist!")        
+        print(f"\n{titulo} - {artista} adicionada a playlist!")
+
+    def apg_mus(self):
+        titulo = input("\nNome da música: ")
+        artista = input("Nome do artista: ")
+        current_node = self.head
+        while current_node:
+            if current_node.titulo == titulo and current_node.artista == artista:
+                if current_node.prev:
+                    current_node.prev.next = current_node.next
+                else:
+                    self.head = current_node.next
+                if current_node.next:
+                    current_node.next.prev = current_node.prev
+                else:
+                    self.tail = current_node.next
+                print(f"\n{titulo} - {artista} deletada da playlist!")
+                return
+            current_node = current_node.next
+        print("\nA música não está na playlist!")
+                                        
