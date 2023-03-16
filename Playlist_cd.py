91a751d0fa83fb10dd3b471727945a4126c25c7b
Noemy Torres <noemy_torres9@hotmail.com>
2023-03-15 23:21:14 -0300

    Adicionado códigos ANSI para colorir o terminal, correção final de texto

--- Playlist_cd.py
+++ Playlist_cd.py
@@ -15,28 +15,28 @@ class Playlist:
     def adc_mus(self):
         titulo = input("\nNome da música: ")
         while not titulo.strip():
-            print("\nEntrada inválida. Tente novamente:")
+            print("\n\033[1;33;40mEntrada inválida. Tente novamente:")
             print("\nDeseja continuar ou voltar para o menu? (c/m)")
-            opc1 = input("\nDigite sua opção: ")
+            opc1 = input("\n\033[1;37;40mDigite sua opção: ")
             if opc1 == 'c':
                 titulo = input("\nNome da música: ")
             elif opc1 == 'm':
-                print("\nVoltando para o menu...")
+                print("\n\033[1;33;40mVoltando para o menu...")
                 return
             else:
-                print("\nOpção inválida!")    
+                print("\n\033[1;31;40mOpção inválida!")    
         artista = input("\nNome do artista: ")
         while not artista.strip():
-            print("\nEntrada inválida. Tente novamente:")
+            print("\n\033[1;33;40mEntrada inválida. Tente novamente:")
             print("\nDeseja continuar ou voltar para o menu? (c/m)")
-            opc2 = input("\nDigite sua opção: ")
+            opc2 = input("\n\033[1;37;40mDigite sua opção: ")
             if opc2 == 'c':
                 artista = input("\nNome do artista: ")
             elif opc2 == 'm':
-                print("\nVoltando para o menu...")
+                print("\n\033[1;33;40mVoltando para o menu...")
                 return
             else:
-                print("\nOpção inválida!")    
+                print("\n\033[1;31;40mOpção inválida!")    
         new_node = Musica(titulo, artista)
         if not self.head:
             self.head = new_node
@@ -45,33 +45,33 @@ class Playlist:
             new_node.prev = self.tail
             self.tail.next = new_node
             self.tail = new_node
-        print(f"\n{titulo} - {artista} adicionada a playlist!")
+        print(f"\n\033[1;32;40m{titulo} - {artista} adicionada a playlist!")
 
     def apg_mus(self):
         titulo = input("\nNome da música: ")
         while not titulo.strip():
-            print("\nEntrada vazia. Tente Novamente:")
+            print("\n\033[1;33;40mEntrada vazia. Tente Novamente:")
             print("\nDeseja continuar ou voltar para o menu? (c/m)")
-            opc3 = input("\nDigite sua opção: ")
+            opc3 = input("\n\033[1;37;40mDigite sua opção: ")
             if opc3 == 'c':
                 titulo = input("\nNome da música: ")
             elif opc3 == 'm':
-                print("\nVoltando para o menu...")
+                print("\n\033[1;33;40mVoltando para o menu...")
                 return
             else:
-                print("\nOpção inválida")    
+                print("\n\033[1;31;40mOpção inválida")    
         artista = input("\nNome do artista: ")
         while not artista.strip():
-            print("\nEntrada vazia. Tente Novamente:")
+            print("\n\033[1;33;40mEntrada vazia. Tente Novamente:")
             print("\nDeseja continuar ou voltar para o menu? (c/m)")
-            opc4 = input("\nDigite sua opção: ")
+            opc4 = input("\n\033[1;37;40mDigite sua opção: ")
             if opc4 == 'c':
                 artista = input("\nNome do artista: ")
             elif opc4 == 'm':
-                print("\nVoltando para o menu...")
+                print("\n\033[1;33;40mVoltando para o menu...")
                 return
             else:
-                print("\nOpção inválida!")    
+                print("\n\033[1;31;40mOpção inválida!")    
         current_node = self.head
         while current_node:
             if current_node.titulo == titulo and current_node.artista == artista:
@@ -83,24 +83,24 @@ class Playlist:
                     current_node.next.prev = current_node.prev
                 else:
                     self.tail = current_node.next
-                print(f"\n{titulo} - {artista} deletada da playlist!")
+                print(f"\n\033[1;31;40m{titulo} - {artista} deletada da playlist!")
                 return
             current_node = current_node.next
-        print("\nA música não está na playlist!")
+        print("\n\033[1;33;40mA música não está na playlist!")
 
     def ver_playlist(self):
         if not self.head:
-            print("\nA playlist está vazia.")
+            print("\n\033[1;33;40mA playlist está vazia.")
             return
         current_node = self.head
-        print("\nMúsicas na playlist:")
+        print("\n\033[1;34;40mMúsicas na playlist:")
         while current_node:
-            print(f"\n{current_node.titulo} - {current_node.artista}")
+            print(f"\n\033[1;32;40m{current_node.titulo} - {current_node.artista}")
             current_node = current_node.next
 
     def shuffle(self):
         if not self.head:
-            print("\nA playlist está vazia!")
+            print("\n\033[1;33;40mA playlist está vazia!")
             return
         musicas = []
         current_node = self.head
@@ -115,41 +115,41 @@ class Playlist:
         for i in range(len(musicas)-1):
             musicas[i].next = musicas[i+1]
             musicas[i+1].prev = musicas[i]
-        print("\nPlaylist embaralhada com sucesso!")
+        print("\n\033[1;32;40mPlaylist embaralhada com sucesso!")
 
     def busc_mus(self):
-        search_str = input("\nO que está procurando?: ")
-        while not search_str.strip():
-            print("\nEntrada vazia! Tente novamente: ")
+        buscar = input("\nO que está procurando?: ")
+        while not buscar.strip():
+            print("\n\033[1;33;40mEntrada vazia! Tente novamente: ")
             print("\nDeseja continuar ou voltar para o menu? (c/m)")
-            opc5 = input("\nDigite sua opção: ")
+            opc5 = input("\n\033[1;37;40mDigite sua opção: ")
             if opc5 == 'c':
-                search_str = input("\nO que está procurando?: ")
+                buscar = input("\n\033[1;37;40mO que está procurando?: ")
             elif opc5 == 'm':
-                print("\nVoltando para o menu...")
+                print("\n\033[1;33;40mVoltando para o menu...")
                 return
-        print("\nMúsicas na playlist:")
+        print("\n\033[1;34;40mMúsicas na playlist:")
         current_node = self.head
-        if current_node == None: print("\nA playlist está vazia!")
+        if current_node == None: print("\n\033[1;33;40mA playlist está vazia!")
         while current_node:
-            if search_str in current_node.titulo or search_str in current_node.artista:
-                print(f"\n{current_node.titulo} - {current_node.artista}")
+            if buscar in current_node.titulo or buscar in current_node.artista:
+                print(f"\n\033[1;32;40m{current_node.titulo} - {current_node.artista}")
             current_node = current_node.next    
 
 
 playlist = Playlist()
 
 while True:
-    print("\n=====================================")
+    print("\n\033[1;36;40m=====================================")
     print("                MENU:")
     print("\n1 - Adicionar música")
     print("2 - Deletar música")
     print("3 - Exibir playlist")
     print("4 - Embaralhar playlist")
     print("5 - Buscar na playlist")
-    print("5 - Sair do menu")
+    print("6 - Sair do menu")
     print("\n=====================================")
-    opc = int(input("\nDigite o valor da sua opção: "))
+    opc = int(input("\n\033[1;37;40mDigite o valor da sua opção: "))
     if opc == 1:
         playlist.adc_mus()
     elif opc == 2:
@@ -161,7 +161,9 @@ while True:
     elif opc == 5:
         playlist.busc_mus()            
     elif opc == 6:
-        print("\nAté a próxima!")
+        print("\n\033[1;34;40mAté a próxima!")
+        print("\033[1;37;40m")
         break
     else:
-        print("\nOpção Inválida! Tente Novamente.")    
+        print("\n\033[1;31;40mOpção Inválida! Tente Novamente.")
+        print("\033[1;37;40m")    
